4ff85a2334bbe16b9aeb379fd4f0429584542ccd
iforgot-my-username <garibay2018paul@gmail.com>
2022-02-11 17:37:05 +0800

    Product card v3

--- src/ProductSection.tsx
+++ src/ProductSection.tsx
@@ -43,25 +43,29 @@ export default function ProductSection({ products }: ProductSectionProps) {
         setsaleDate('');
     }
 
-    const items: JSX.Element[] = [];
-    const itemPrices: { [key: string]: number } = {}
+    const filteredItems: { [key: string]: ProductProps } = {}
 
-    products.forEach(({ name, price, event, pictureSrc }: ProductProps, index: number) => {
-        const key = name + '->' + pictureSrc;
-        if (itemPrices[key] === undefined) {
-            items.push(
-                <ProductCard name={name} price={price} pictureSrc={pictureSrc} event={event} onClick={setState} key={index}></ProductCard>
-            );
+    products.forEach((product: ProductProps) => {
+        const { name, pictureSrc } = product
+        const productKey = name + '->' + pictureSrc;
+
+        if (filteredItems[productKey] === undefined) {
+            filteredItems[productKey] = product;
         } else {
-            items.push(
-                <ProductCard name={name} price={price} pictureSrc={pictureSrc} event={event} onClick={setState} oldPrice={itemPrices[key]} key={index}></ProductCard>
-            );
-        }
-        itemPrices[key] = price;
+            const oldPrice = filteredItems[productKey].price
 
-        // console.log(Object.values(productss))
+            filteredItems[productKey] = { ...product, oldPrice }
+        }
     });
-    console.log(Object.values(itemPrices))
+
+
+    const items: JSX.Element[] = [];
+
+    Object.values(filteredItems).forEach(({ name, price, oldPrice, event, pictureSrc }: ProductProps, index: number) => {
+        items.push(
+            <ProductCard name={name} price={price} oldPrice={oldPrice} pictureSrc={pictureSrc} event={event} onClick={setState} key={index}></ProductCard>
+        );
+    });
 
     return (
         <div className="container-fluid section-div shadow pt-3">
