fc101271081e07ae26d6532e88cb7305c55e0ec5
iforgot-my-username <garibay2018@gmail.com>
2022-02-16 07:01:25 +0800

    Apply lesssons

--- src/ProductSection.tsx
+++ src/ProductSection.tsx
@@ -1,91 +1,99 @@
-import React, { useState } from 'react';
-import DurationTimer from './DurationTimer';
-import ProductCard from './ProductCard';
-import './ProductSection.css';
+import React, { useState } from "react";
+import DurationTimer from "./DurationTimer";
+import ProductCard from "./ProductCard";
+import "./ProductSection.css";
 
 interface ProductProps {
-    name: string;
-    price: number;
-    oldPrice?: number;
-    event: Date;
-    pictureSrc: string;
+  name: string;
+  price: number;
+  oldPrice?: number;
+  event: Date;
+  pictureSrc: string;
 }
 
 interface ProductSectionProps {
-    products: ProductProps[];
+  products: ProductProps[];
 }
 
 export default function ProductSection({ products }: ProductSectionProps) {
-    const [saleDate, setsaleDate] = useState('');
+  const [saleDate, setsaleDate] = useState("");
 
+  function setState(date: Date) {
+    const strDate = date.toString();
 
-    function setState(date: Date) {
-        const strDate = date.toString();
-
-        if (saleDate === '') {
-            setsaleDate(strDate);
-
-        } else {
-            new Promise(
-                (resolve, reject) => {
-                    setsaleDate('');
-                    resolve(true);
-                }
-            ).then(
-                () => {
-                    setsaleDate(strDate);
-                }
-            );
-        }
+    if (saleDate === "") {
+      setsaleDate(strDate);
+    } else {
+      new Promise((resolve, reject) => {
+        setsaleDate("");
+        resolve(true);
+      }).then(() => {
+        setsaleDate(strDate);
+      });
     }
+  }
 
-    function Close() {
-        setsaleDate('');
-    }
+  function Close() {
+    setsaleDate("");
+  }
 
-    const items: JSX.Element[] = [];
-    const itemPrices: { [key: string]: number } = {}
+  const items: JSX.Element[] = [];
+  const itemPrices: { [key: string]: number } = {};
 
-    products.forEach(({ name, price, event, pictureSrc }: ProductProps, index: number) => {
-        const key = name + '->' + pictureSrc;
-        if (itemPrices[key] === undefined) {
-            items.push(
-                <ProductCard name={name} price={price} pictureSrc={pictureSrc} event={event} onClick={setState} key={index}></ProductCard>
-            );
-        } else {
-            items.push(
-                <ProductCard name={name} price={price} pictureSrc={pictureSrc} event={event} onClick={setState} oldPrice={itemPrices[key]} key={index}></ProductCard>
-            );
-        }
-        itemPrices[key] = price;
-
-    });
-    console.log(Object.values(itemPrices))
-
-
-
-
-    // Object.values(filteredItems).forEach(({ name, price, oldPrice, event, pictureSrc }: ProductProps, index: number) => {
-    //     items.push(
-    //         <ProductCard name={name} price={price} oldPrice={oldPrice} pictureSrc={pictureSrc} event={event} onClick={setState} key={index}></ProductCard>
-    //     );
-    // });
-
-    return (
-        <div className="container-fluid section-div shadow pt-3">
-            <div className="col ">
-                <div className="row ">
-                    <div className="d-flex flex-row-reverse px-5 pb-3 overflow-auto">
-                        < div className="align-self-top">
-                            {saleDate && <button className="btn btn-danger shadow rounded-pill" onClick={Close}>X</button>}
-                        </div>
-                        {saleDate && <DurationTimer end={new Date(saleDate)} />}
-                    </div>
-                </div>
-                <div className="d-flex product-div overflow-auto">
-                    {items}
-                </div>
+  return (
+    <div className="container-fluid section-div shadow pt-3">
+      <div className="col ">
+        <div className="row ">
+          <div className="d-flex flex-row-reverse px-5 pb-3 overflow-auto">
+            <div className="align-self-top">
+              {saleDate && (
+                <button
+                  className="btn btn-danger shadow rounded-pill"
+                  onClick={Close}
+                >
+                  X
+                </button>
+              )}
             </div>
-        </div >
-    );
+            {saleDate && <DurationTimer end={new Date(saleDate)} />}
+          </div>
+        </div>
+        <div className="d-flex product-div overflow-auto">
+          {products.map(
+            (
+              { name, price, event, pictureSrc }: ProductProps,
+              index: number
+            ) => {
+              const key = name + "->" + pictureSrc;
+              itemPrices[key] = price;
+              if (itemPrices[key] === undefined) {
+                return (
+                  <ProductCard
+                    name={name}
+                    price={price}
+                    pictureSrc={pictureSrc}
+                    event={event}
+                    onClick={setState}
+                    key={index}
+                  ></ProductCard>
+                );
+              }
+
+              return (
+                <ProductCard
+                  name={name}
+                  price={price}
+                  pictureSrc={pictureSrc}
+                  event={event}
+                  onClick={setState}
+                  oldPrice={itemPrices[key]}
+                  key={index}
+                ></ProductCard>
+              );
+            }
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
